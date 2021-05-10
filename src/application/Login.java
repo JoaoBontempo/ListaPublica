e8c153c4642fd937b8738f503d45013625d55422
João Vitor Bontempo <joao.vitor.bontempo19@gmail.com>
2021-05-10 16:06:43 -0300

    Update
    Adicionando tela de login

--- src/application/Login.java
+++ src/application/Login.java
@@ -3,18 +3,23 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
-public class Main extends Application {
+public class Login extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Tela.fxml"));
-			Scene scene = new Scene(root,400,400);
+			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("telaLogin.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Image icon = new Image("Recursos/logo.png");
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Lista Pública");
+			primaryStage.getIcons().add(icon);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/classes/Banco.java
@@ -0,0 +1,79 @@
+package classes;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+
+public class Banco {
+	
+	//Credenciais do banco
+	private static String ip = "";
+	private static String banco = "";
+	private static String usuario = "";
+	private static String senha = "";
+	private static String porta = "";
+	
+	//Biblioteca
+	private final static String DRIVE = "com.mysql.cj.jdbc.Driver";
+	
+	//Variáveis do banco
+	private static Connection conexao = null;
+	private static Statement statement = null;
+	private static ResultSet resultados = null;
+	
+	public static ResultSet getReader()
+	{
+		return resultados;
+	}
+	
+	static void MessageBoxShow(String titulo, String conteudo, AlertType tipo)
+	{
+		Alert alert = new Alert(tipo);
+		alert.setTitle(titulo);
+		alert.setHeaderText(null);
+		alert.setContentText(conteudo);
+		alert.showAndWait();
+	}
+	
+	//String de conexao
+	private static String stringConexao = String.format("jdbc:mysql://%s:%s/%s", ip, porta, banco);
+	
+	
+	public static void Conectar() throws ClassNotFoundException, SQLException
+	{
+		try
+		{
+			Class.forName(DRIVE);
+			conexao = DriverManager.getConnection(stringConexao, usuario, senha);
+			statement = conexao.createStatement();
+		}
+		catch (Exception erro)
+		{
+			MessageBoxShow("Erro ao conectar ao banco", erro.getMessage(), AlertType.INFORMATION);
+		}
+	}
+	
+	public static ResultSet InserirQueryReader(String query) throws SQLException
+	{
+		resultados = statement.executeQuery(query);
+		statement = conexao.createStatement();
+		return resultados;
+	}
+	
+	public static void InserirQuery(String query) throws SQLException
+	{
+		statement.executeUpdate(query);
+	}
+	
+	public static void Desconectar() throws SQLException
+	{
+		conexao.close();
+	}
+	
+	
+}
