adc0c8dcca496f749e94998b50b471d667f1b794
João Vitor Bontempo <joao.vitor.bontempo19@gmail.com>
2021-05-12 17:32:58 -0300

    Update
    - Adicionando bibliotecas da API de estados/cidades (geonames)

--- src/application/Dashboard.java
+++ src/application/Dashboard.java
@@ -1,5 +1,10 @@
 package application;
 	
+import org.geonames.Toponym;
+import org.geonames.ToponymSearchCriteria;
+import org.geonames.ToponymSearchResult;
+import org.geonames.WebService;
+
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -19,6 +24,20 @@ public class Dashboard extends Application {
 	
 	@FXML
     private TextField txtPesquisar;
+	
+	
+	//Método 'onLoad'
+	public void initialize() throws Exception
+	{
+		WebService.setUserName("JoaoBontempo"); // add your username here
+
+		ToponymSearchCriteria searchCriteria = new ToponymSearchCriteria();
+		searchCriteria.setQ("brazil");
+		ToponymSearchResult searchResult = WebService.search(searchCriteria);
+		for (Toponym toponym : searchResult.getToponyms()) {
+			System.out.println(toponym.getName()+" "+ toponym.getCountryName());
+		}
+	}
 		
 	@Override
 	public void start(Stage primaryStage) {
