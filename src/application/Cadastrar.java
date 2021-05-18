5bb7819e9bae05d83ef8931045ba37e143f2869c
VictorSantaterra <67128209+VictorSantaterra@users.noreply.github.com>
2021-05-17 22:42:15 -0300

    Cadastro Update

--- src/application/Cadastrar.java
+++ src/application/Cadastrar.java
@@ -3,6 +3,8 @@ package application;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import com.sun.javafx.geom.Point2D;
+
 import classes.Banco;
 import classes.Util;
 import classes.Validacao;
@@ -12,10 +14,12 @@ import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
+import javafx.scene.control.Tooltip;
 import javafx.scene.input.DragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
@@ -29,7 +33,6 @@ public class Cadastrar extends Application{
 		launch(args);
 
 	}
-
 	@FXML
 	private TextField txtUsuario;
 
@@ -44,20 +47,19 @@ public class Cadastrar extends Application{
 
 	@FXML
 	private PasswordField txtConfirmarSenha;
-
 	@FXML
-	private PasswordField txtCPFouCNPJ;
+	private TextField txtCPFouCNPJ;
 
 	@FXML
 	private TextField txtNome;
 
 	@FXML
-	void RealizarCadastro(ActionEvent event) {
-		if(ValidarCampos()) {
-			realizarCadastro(tipo);
-		}
+	void realizarCadastro(ActionEvent event) {
 
+		if(ValidarCampos()) 
+			realizarCadastro(tipo);
 	}
+
 	public boolean tipo = false;
 	boolean ValidarCampos() {
 
@@ -65,18 +67,25 @@ public class Cadastrar extends Application{
 			return false;
 		if (!Validacao.verificarTextField(txtUsuario))
 			return false;
+		if (!Validacao.verificarTextField(txtEmail))
+			return false;
+		if (!Validacao.verificarTextField(txtCPFouCNPJ))
+			return false;
 		if (!Validacao.verificarTextField(txtSenha))
 			return false;
 		if (!Validacao.verificarTextField(txtConfirmarSenha))
 			return false;
-		if (!Validacao.verificarTextField(txtCPFouCNPJ))
+
+		if(txtUsuario.getText().contains("@") || txtUsuario.getText().matches("[0-9]+")) {
+			Util.MessageBoxShow("Erro ao Cadastrar", "O Usuário inderido é inválido", AlertType.ERROR);
 			return false;
-		if (!Validacao.verificarTextField(txtEmail))
+		}
+
+		if(!Validacao.validarEmail(txtEmail.getText())) {
+			Util.MessageBoxShow("Erro ao Cadastrar", "O E-mail inserido é inválido", AlertType.ERROR);
 			return false;
-		if(!Validacao.validarEmail(txtEmail.getText()))
-			return false;
-		if(txtUsuario.getText().contains("@") || txtUsuario.getText().matches("[0-9]+"))
-			return false;
+		}
+
 
 		if(txtCPFouCNPJ.getText().length() == 14) {
 			tipo = true; //tipo cnpj
@@ -91,23 +100,34 @@ public class Cadastrar extends Application{
 	}
 
 	void realizarCadastro(boolean tipo) {
-
+     
 		try {
 			
-			if(tipo) {
-				Banco.InserirQuery(String.format("INSERT INTO parceiro(id,nome,usuario,tipo,cnpj,email,senha) VALUES(default, %s,%s,%s,%s,%s,%s)",
-						txtNome.getText(), txtUsuario.getText(),tipo,  txtCPFouCNPJ.getText(), txtEmail.getText(), Util.criptografarSenha(txtSenha.getText())));
-			}
-			else {
+			if(txtSenha.getText().equals(txtConfirmarSenha.getText()))
+			{
+				if(tipo) {
+					Banco.InserirQuery(String.format("INSERT INTO parceiro(id,nome,usuario,tipo,cnpj,email,senha) VALUES(default,'%s','%s','%s','%s','%s','%s')",
+							txtNome.getText(), txtUsuario.getText(),"1",  txtCPFouCNPJ.getText(), txtEmail.getText(), Util.criptografarSenha(txtSenha.getText())));
+				}
+				else {
 
-				Banco.InserirQuery(String.format("INSERT INTO parceiro(id,nome,usuario,tipo,cpf,email,senha) VALUES(default, %s,%s,%s,%s,%s,%s)",
-						txtNome.getText(), txtUsuario.getText(),tipo ,txtCPFouCNPJ.getText(), txtEmail.getText(), Util.criptografarSenha(txtSenha.getText())));
+					Banco.InserirQuery(String.format("INSERT INTO parceiro(id,nome,usuario,tipo,cpf,email,senha) VALUES(default,'%s','%s','%s','%s','%s','%s')",
+							txtNome.getText(), txtUsuario.getText(),"0",txtCPFouCNPJ.getText(), txtEmail.getText(), Util.criptografarSenha(txtSenha.getText())));
+				}
+
+				Util.MessageBoxShow("Cadastar", "Cadastro realizado com sucesso", AlertType.INFORMATION);
 			}
+			else { 
+				
+				Util.MessageBoxShow("Erro ao Cadastrar", "As senhas inseridas não correspondem", AlertType.ERROR);
+				txtConfirmarSenha.requestFocus();
+
+			 
 			
-			Util.MessageBoxShow("Cadastar", "Cadastro realizado com sucesso", AlertType.INFORMATION);
+			}
 		}
 		catch (Exception ex){
-			
+
 			Util.MessageBoxShow("Cadastar", "Erro ao Cadastar: " + ex.getMessage(), AlertType.ERROR);
 		}
 
