5a1b4c9aebcf2cecba9761d977498d581d47a01b
JoaoBontempo <joao.vitor.bontempo19@gmail.com>
2021-05-27 19:55:06 -0300

    Update

--- Sistema Moderador/SistemaModerador/SistemaModerador/Classes/Banco.cs
+++ Sistema Moderador/SistemaModerador/SistemaModerador/Classes/Banco.cs
@@ -13,7 +13,7 @@ namespace SistemaModerador.Classes
 
         private static MySqlCommand cmd;
 
-        private static string parametrosBancoAWS = "server=dblistapublica.ccenmvdsqpiw.us-east-2.rds.amazonaws.com; Port=3306; uid=gerenciamento;pwd=S3nh4F0rt3; database=db_lista_publica; convert zero datetime=True";
+        private static string parametrosBancoAWS = "server=4.tcp.ngrok.io; Port=16357; uid=root;pwd=root; database=db_lista_publica; convert zero datetime=True";
         public static int linhasAfetadas { get; private set; }
         public static void AbreConexao()
         {
